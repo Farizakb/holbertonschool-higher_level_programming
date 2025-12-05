b56ff0a36fda1c2a2dd4f6fd40dfcb0e009f792f
Fariz <farizakbarzada@gmail.com>
2025-12-05 17:07:26 +0000

    commit6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers"""
+    for row in matrix:
+        for idx, num in enumerate(row):
+            if idx != 0:
+                print(" ", end="")
+            print("{}".format(num), end="")
+        print()
