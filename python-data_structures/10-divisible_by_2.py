0835a81d17fd7b02209ce15399d997de5bc5fe64
Fariz <farizakbarzada@gmail.com>
2025-12-05 17:20:15 +0000

    commit 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Finds all multiples of 2 in a list.
+    
+    Returns a new list of True/False values depending on
+    whether each integer is divisible by 2.
+    """
+    return [num % 2 == 0 for num in my_list]
