07647682dddbe1b108c9706d7abef6672bb04f23
Fariz <farizakbarzada@gmail.com>
2025-12-05 17:03:23 +0000

    commit4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replace an element at a specific index without modifying the original list"""
+    """Replace an element at a specific index without modifying """
     new_list = my_list[:]  # create a copy of the list
     if 0 <= idx < len(new_list):
         new_list[idx] = element
