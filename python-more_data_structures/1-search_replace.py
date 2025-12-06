c82e9ba9e48f43f91439618788d924e978f906b2
Fariz <farizakbarzada@gmail.com>
2025-12-06 12:47:16 +0000

    commit 1.2

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,4 +3,3 @@ def search_replace(my_list, search, replace):
     """Return a new list with all occurrences of
     'search' replaced by 'replace'."""
     return [replace if x == search else x for x in my_list]
-
