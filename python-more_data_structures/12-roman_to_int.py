aeee23f152c9b1c7d4088cb685e9730cba8037ff
Fariz <farizakbarzada@gmail.com>
2025-12-06 13:22:15 +0000

    commit 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """Convert a Roman Numeral to an Integer."""
+    roman_to_int_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    if number is None or not isinstance(roman_string, str):
+        return 0
+    total = 0
+    prev_value = 0
+    for char in reversed(roman_string):
+        value = roman_to_int_map.get(char, 0)
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    return total
