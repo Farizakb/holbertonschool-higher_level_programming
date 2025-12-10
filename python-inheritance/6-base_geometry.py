68e305b1754e7e88afb94b6acfefc6dcd0aca80d
farizakb <farizakbarzada@gmail.com>
2025-12-10 20:46:59 +0400

    commit 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""6-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Base class for geometry"""
+
+    def area(self):
+        """Public instance method that is not implemented"""
+        raise Exception("area() is not implemented")
