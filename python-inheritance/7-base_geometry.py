b4006fa7125f2f7b74eaba9410a8e7e57a890742
farizakb <farizakbarzada@gmail.com>
2025-12-10 20:50:27 +0400

    commit 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""7-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Base class for geometry"""
+
+    def area(self):
+        """Public instance method that is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is a positive integer"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
