90d5232030657605fad9030beba420e02651a348
farizakb <farizakbarzada@gmail.com>
2025-12-12 19:37:16 +0400

    commit 8.2

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-Function that returns the dictionary description with simple data st
-(list, dictionary, string, integer, boolean) for JSON serialization of an o.
+Function that returns the dictionary description with simple data structure
+(list, dictionary, string, integer, boolean) for JSON serialization of an obje
 """
+
 
 def class_to_json(obj):
     """
-    Returns the dictionary representation of obj suitable for JSON serializat.
+    Returns the dictionary representation of obj suitable for JSON serializati.
 
     Args:
         obj: instance of a class
