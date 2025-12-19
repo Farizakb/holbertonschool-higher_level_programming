3d87c97e3c3884756874f1187fe34ace34d188ca
farizakb <farizakbarzada@gmail.com>
2025-12-19 19:16:30 +0400

    commit 11.1

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,9 +1,11 @@
 -- 11-genre_id_all_shows.sql
 -- Script that lists all shows with their genre_id
--- Shows without a genre display NULL
 -- Results are sorted by tv_shows.title and tv_show_genres.genre_id
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT
+  tv_shows.title AS title,
+  tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
