c11ce2720b04bd816b540fc64883e8151d3c19d6
farizakb <farizakbarzada@gmail.com>
2025-12-19 19:13:36 +0400

    commit 10.1

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,10 @@
 -- Script that lists all shows with at least one linked genre
 -- Results are sorted by tv_shows.title and tv_show_genres.genre_id
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT
+  tv_shows.title AS title,
+  tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+INNER JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
