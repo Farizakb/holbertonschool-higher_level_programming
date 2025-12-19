acce9392183b8b1492c9129feaa2e989a595fa33
farizakb <farizakbarzada@gmail.com>
2025-12-19 19:08:47 +0400

    commit 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,12 @@
+-- 8-cities_of_california_subquery.sql
+-- Script that lists all cities of California without using JOIN
+-- Results are ordered by cities.id in ascending order
+
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
